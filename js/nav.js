5e426a80573712ae3023858abb382439362f49de
nova <85376541+heopc@users.noreply.github.com>
2022-11-01 08:20:29 +0700

    update

---
+++ js/nav.js
@@ -0,0 +1,79 @@
+function nav(){
+    return "<div class=\"container\">\n" +
+        "            <div class=\"row col md-3 col-md-offset-5 logo\">\n" + 
+        "                <a href=\"Patek Phillipee.html\">\n" +
+        "                <img src=\"imgs/Patek-Philippe-Logo.png \" height=\"150\" width=\"200\"/>\n" +
+        "                </a>\n" +
+        "            </div>\n" +
+        "            <div class=\"row col-md-12 menu\">\n" +
+        "                <div class=\"a-menu pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"thuong-hieu.html\" >\n" +
+        "                            <p>\n" +
+        "                                THƯƠNG HIỆU\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "                <div class=\"a-menu col-md-2 pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"dong-ho-nam.html\">\n" +
+        "                            <p>\n" +
+        "                                ĐỒNG HỒ NAM\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "                <div class=\"a-menu pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"dong-ho-nu.html\">\n" +
+        "                            <p>\n" +
+        "                                ĐỒNG HỒ NỮ\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2 a-menu pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"#\">\n" +
+        "                            <p>\n" +
+        "                                ĐỒNG HỒ CẶP ĐÔI\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2 a-menu pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"#\">\n" +
+        "                            <p>\n" +
+        "                                SẢN PHẨM HOT\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-1 a-menu pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"#\">\n" +
+        "                            <p>\n" +
+        "                                SALE\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-2 a-menu pull-left\">\n" +
+        "                    <label>\n" +
+        "                        <a href=\"#\">\n" +
+        "                            <p>\n" +
+        "                                SPECIAL EVENT\n" +
+        "                            </p>\n" +
+        "                        </a>\n" +
+        "                    </label>\n" +
+        "                </div>\n" +
+        "\n" +
+        "\n" +
+        "            </div>\n" +
+        "\n" +
+        "        </div>"
+}
+var nav1 = document.getElementById("nav");
+nav1.innerHTML = nav();
